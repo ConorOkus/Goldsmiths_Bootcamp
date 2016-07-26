bc52928b43145f1eb545aaa2211b4741c16aaea5
Conor Okus <conor.okus@hotmail.co.uk>
2016-07-26 16:32:10 +0100

    revision one

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* Toggle between adding and removing the "responsive" class to primary nav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("primary-nav")[0].classList.toggle("responsive");
+}
+
