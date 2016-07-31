39461b629e180d4d83784d5e3d19bdcf0471c1a8
Conor Okus <conor.okus@hotmail.co.uk>
2016-07-31 17:45:24 +0100

    Rename JS Responisive Function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 // Toggle between adding and removing the "responsive" class to primary nav when the user clicks on the icon 
-function myFunction() {
+function mainMenu() {
     document.getElementsByClassName("primary-nav")[0].classList.toggle("responsive");
 }
 
-// Check if user has entered a valid number and produce error message
-document.registerform.onsubmit=function() {
- 		
- 		var theNumber = document.registerform.num.value;
- 		
- 		if (isNaN(theNumber)) {
- 			document.getElementById("errormessage").innerHTML = "(Enter a valid number)";
- 			return false;
- 		} 
- 		
- 	}
+
 
