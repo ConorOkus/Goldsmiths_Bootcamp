e7e3defe6ddd05f127b80c077268a231fd8aa793
Conor Okus <conor.okus@hotmail.co.uk>
2016-08-08 14:09:53 +0100

    Thank you page

---
+++ thanks.php
@@ -0,0 +1,9 @@
+<?php include 'templates/header.php'; ?>
+
+    <main>
+        <h2>Registration Confirmation</h2>
+        <p>Thank you for registering to attend Goldsmiths Bootcamp.
+            An email with the confirmation details has been sent to the address you supplied.</p>
+    </main>
+    
+<?php include 'templates/footer.php'; ?>
